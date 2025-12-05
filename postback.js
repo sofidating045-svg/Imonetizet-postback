90d0ac855f4c68339f049334f5a319355ea74318
sofidating045-svg <sofidating045@gmail.com>
2025-12-05 11:18:03 +0700

    Add files via upload

---
+++ postback.js
@@ -0,0 +1,31 @@
+exports.handler = async (event) => {
+  const params = event.queryStringParameters;
+
+  const botToken = "8479557204:AAFl7evg5QA_0Q5vLxpyWU8KCJSxSfnAfug";
+  const chatId = "-1003319175155";
+
+  const campaign = params.campaign || "-";
+  const subid = params.sub_id_1 || "-";
+  const country = params.country || "-";
+  const payout = params.payout || "-";
+  const platform = params.platform || "-";
+  const ip = params.ip || "-";
+
+  const message =
+    "🏢 : " + campaign + "\n" +
+    "🦹 : " + subid + "\n" +
+    "🌏 : " + country + "\n" +
+    "💵 : " + payout + "\n" +
+    "📱 : " + platform + "\n" +
+    "📍 : https://scamalytics.com/ip/" + ip;
+
+  const url = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`;
+
+  const fetch = (await import("node-fetch")).default;
+  await fetch(url);
+
+  return {
+    statusCode: 200,
+    body: "OK"
+  };
+};
